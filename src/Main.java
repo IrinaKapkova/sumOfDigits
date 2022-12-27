56dea14588f4e823040ecacd04f34fe1aa0c4105
Ирина Капкова <kapiral@rambler.ru>
2022-12-27 23:06:21 +0400

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+       System.out.println("Введите число: ");
+
+       if (scan.hasNextLong()) {
+           long n = scan.nextLong();
+           System.out.println("Сумма всех цифр введенного числа равна: "+sumOfDigits (n));
+       } else {
+           System.out.println("Извините, но это явно не число. Перезапустите программу и попробуйте снова!");
+       }
+
+   }
+    public static long sumOfDigits (long n) {
+        int sum=0;
+        while (n !=0){
+            sum+=n%10;
+            n/=10;
+        }
+        return Math.abs(sum);
     }
 }
